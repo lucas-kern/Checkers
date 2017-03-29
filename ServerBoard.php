149f679e2852716648241bbe0837a7d6bbaf8e30
lucas-kern <lkern@iastate.edu>
2017-03-28 23:47:12 -0500

    Menu added

--- ServerBoard.php
+++ ServerBoard.php
@@ -4,19 +4,18 @@
 //    array(0, 1, 2),
 //    array(3, 4, 5),
 //);
-
 $arr = array_fill(0, 8, array_fill(0,8,0));
-
 //echo $arr[7][7];
+
+
 
 function drawTable($rows,$cols,$arr){
 echo "<table border='1'>"; 
 for($tr=0;$tr<$rows;$tr++){
-    if ($cols >= 0) {
+    if ($cols > 0) {
     echo "<tr>"; 
-        echo "<td align='center'></td>";
         for($td=0;$td<$cols;$td++){ 
-               echo "<td align='center' name = 'book' id= 6>".$arr[$tr][$td]."</td>"; 
+               echo "<td align='center' name = '".$tr."' id=".$td.">".$arr[$tr][$td]."</td>"; 
         } 
     echo "</tr>"; 
     } 
